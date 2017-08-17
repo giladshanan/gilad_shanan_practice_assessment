c0745482e37a7bd24469ae9e447cd85839587567
Gilad Shanan <giladshanan@gmail.com>
2017-08-17 17:45:39 -0500

    create Genie class

---
+++ genie.rb
@@ -0,0 +1,14 @@
+class Genie
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+end
+
+genie1 = Genie.new("Bob", 1234)
+genie2 = Genie.new("Karen", 3945)
+genie3 = Genie.new("Susan", 509234)
+
+p genie1
+p genie2
+p genie3
